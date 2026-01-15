df7a8e0c138953d6514b868560e5079417fcea0f
Vladimir Angelov <vladimirsfile@gmail.com>
2026-01-15 17:45:59 +0200

    Add a second function square() to the math.js file

--- math.js
+++ math.js
@@ -1,6 +1,17 @@
+// Double function 
+
 function double(n) {
-    return `The doube of ${n} is ${n + n}`;
+    return `The double of ${n} is ${n + n}`;
 }
 let n = 22;
 result = double(n);
-console.log(double(n));
+console.log(result);
+
+// Square function 
+
+function square(num) {
+    return `The square of ${num} is ${num ** 2}`;
+}
+let num = 7;
+output = square(num);
+console.log(output);
